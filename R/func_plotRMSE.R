d12077b69416c892166e498781f52412e5ff95b3
ismaelvbrack <ismaelbrack@hotmail.com>
2021-05-29 21:56:06 -0300

    Update function to plot optimal J in a separated frame

--- R/func_plotRMSE.R
+++ R/func_plotRMSE.R
@@ -1,6 +1,8 @@
 #---------------------------------------------------------------------------------------
-####** function to create figure with ggplot ploting RMSE curves for selected scenario
-#
+####** function to plot RMSE curves for selected scenario
+#####* using ggplot
+
+####* This function uses a object like rel.rmse2 (data.frame with RMSE, J, obs...)
 ####* type= simple: for 1 and 2 obs models
 ####* type= propObs: for different proportions of 2obs protocol based on scenario
 ####* type = effortDiff: 
@@ -12,38 +14,43 @@ plotRMSE <- function(data,data2=NULL,scen,type="simple",complete=FALSE){
   require(grid)
   require(wesanderson)
   
+
+# type="simple", 1obs and 2obs --------------------------------------------
+
   if(type=="simple"){
     i=scen
     data <- data[[i]]
     # arranging needed information
-    my.scale <- (max(data$RMSE) - min(data$RMSE))*.3
-    yax <- c(min(data$RMSE)-my.scale, max(data$RMSE))
+    yax <- c(0, max(data$RMSE))
     
-    opt1 <- as.numeric(as.character(data[which(data$RMSE == min(data[which(data$obs2==0),"RMSE"])),"J"]))
-    opt2 <- as.numeric(as.character(data[which(data$RMSE == min(data[which(data$obs2==1),"RMSE"])),"J"]))
-    
-    opt1.range <- range(as.numeric(as.character(
-      data[which(data$RMSE < (min(data[which(data$obs2==0),"RMSE"])+0.005) & data$obs2==0),"J"] )))
-    opt2.range <- range(as.numeric(as.character(
-      data[which(data$RMSE < (min(data[which(data$obs2==1),"RMSE"])+0.005) & data$obs2==1),"J"] )))
-    
-    Jmin=c(opt1.range[1],opt2.range[1])
-    Jmax=c(opt1.range[2],opt2.range[2])
+    tab <- data.frame(optJ=tapply(data$RMSE, data$obs2,
+                                  function(x) as.numeric(as.character(data[data$RMSE==min(x),"J"]))),
+                      Jmin=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[1]),
+                      Jmax=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[2]),
+                      obs2=unique(data$obs2))
+
     
     if(complete==FALSE){
       # ggplot figure
       fig.out <- 
-        ggplot(data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2)) +
-        geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
-        coord_cartesian(ylim=yax) +
-        theme_classic() + scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
-        theme(axis.title=element_blank(),axis.text=element_text(size=13),legend.position="none") +
-        geom_errorbarh(aes(xmin=Jmin[1],xmax=Jmax[1],y=0.01+(my.scale/2)),size=1,height=my.scale/3,colour="#3B9AB2") +
-        geom_errorbarh(aes(xmin=Jmin[2],xmax=Jmax[2],y=0.01),size=1,height=my.scale/3,colour="#F21A00") +
-        annotation_custom(textGrob("*", gp = gpar(col="#F21A00",fontsize=36)), 
-                          xmin=opt2, xmax=opt2,ymin=0.01, ymax=0.01) +
-        annotation_custom(textGrob("*", gp = gpar(col="#3B9AB2",fontsize=36)), 
-                          xmin=opt1, xmax=opt1,ymin=0.01+(my.scale/2), ymax=0.01+(my.scale/2))
+              ggplot(data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2)) +
+              geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
+              coord_cartesian(ylim=yax) +
+              theme_classic() + scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
+              theme(axis.title=element_blank(),axis.text=element_text(size=13),legend.position="none") 
+      
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax)) + 
+                  geom_point(size=5,shape=18) + geom_errorbarh(size=1.2,height=0.5) +
+                  theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+                  labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+                  theme(panel.background = element_rect(colour = "black"),legend.position="none",
+                        axis.text=element_blank(),axis.title.y=element_blank(),
+                        axis.title.x=element_text(face="bold",size=12)) + 
+                  scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
     }
     
     if(complete==TRUE){
@@ -52,48 +59,64 @@ plotRMSE <- function(data,data2=NULL,scen,type="simple",complete=FALSE){
         ggplot(data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2)) +
         geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
         coord_cartesian(ylim=yax) +
-        theme_classic() + scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
-        theme(axis.text=element_text(size=13),axis.title=element_text(size=16)) + labs(x="Number of visits (J)",color="Model")+#,title=names(data[i])) +
-        geom_errorbarh(aes(xmin=Jmin[1],xmax=Jmax[1],y=yax[1]+(my.scale/2)),size=1,height=my.scale/3,colour="#3B9AB2") +
-        geom_errorbarh(aes(xmin=Jmin[2],xmax=Jmax[2],y=yax[1]),size=1,height=my.scale/3,colour="#F21A00") +
-        annotation_custom(textGrob("*", gp = gpar(col="#F21A00",fontsize=36)), 
-                          xmin=opt2, xmax=opt2,ymin=yax[1], ymax=yax[1]) +
-        annotation_custom(textGrob("*", gp = gpar(col="#3B9AB2",fontsize=36)), 
-                          xmin=opt1, xmax=opt1,ymin=yax[1]+(my.scale/2), ymax=yax[1]+(my.scale/2))
+        theme_classic() + 
+        theme(axis.text=element_text(size=13),axis.title=element_text(size=16),
+              legend.position="top") +
+        scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
+        labs(x="Number of visits (J)",color="Model")
+        
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax)) + 
+        geom_point(size=5,shape=18) + geom_errorbarh(size=1.2,height=0.5) +
+        theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+        labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+        theme(panel.background = element_rect(colour = "black"),legend.position="none",
+              axis.text=element_blank(),axis.title.y=element_blank(),
+              axis.title.x=element_text(face="bold",size=12)) + 
+        scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
       
     }
     
     return(fig.out)
   } # type= "simple"
   
+
+# type="propObs", proportions of 2obs -------------------------------------
+
   if(type=="propObs"){
     i=scen
     data <- data[[i]]
     # arranging needed information
-    my.scale <- (max(data$RMSE) - min(data$RMSE))*.3
-    yax <- c(min(data$RMSE)-my.scale, max(data$RMSE))
+    yax <- c(0, max(data$RMSE))
     
-    optJ <- data.frame(optJ=tapply(data$RMSE, data$obs2,
-                                   function(x) as.numeric(as.character(data[data$RMSE==min(x),"J"]))),
-                       Jmin=tapply(data$RMSE, data$obs2,
-                                   function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[1]),
-                       Jmax=tapply(data$RMSE, data$obs2,
-                                   function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[2]),
-                       obs2=unique(data$obs2),
-                       y=seq(yax[1],min(data$RMSE)-(my.scale/6),length.out=length(unique(data$obs2))))
-    #optJ <- optJ[nrow(optJ):1,]
+    tab <- data.frame(optJ=tapply(data$RMSE, data$obs2,
+                                  function(x) as.numeric(as.character(data[data$RMSE==min(x),"J"]))),
+                      Jmin=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[1]),
+                      Jmax=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[2]),
+                      obs2=unique(data$obs2))
     
     if(complete==FALSE){
-      optJ$y <- seq(0,min(data$RMSE)-.025,length.out=length(unique(data$obs2)))
       # ggplot figure
       fig.out <-
         ggplot(data=data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2)) +
         geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
         coord_cartesian(ylim=yax) +
         theme_classic() + scale_color_manual(values=wes_palette("Zissou1",length(unique(data$obs2)),type="continuous")) + 
-        theme(axis.title=element_blank(),axis.text=element_text(size=18),legend.position="none") +
-        geom_errorbarh(data=optJ,mapping=aes(xmin=Jmin,xmax=Jmax,y=y,colour=obs2),inherit.aes=FALSE,size=1,height=my.scale/4)+
-        geom_text(data=optJ,mapping=aes(x=optJ,y=y,label="*",colour=obs2),inherit.aes=FALSE,size=12,fontface="bold",show.legend=F)
+        theme(axis.title=element_blank(),axis.text=element_text(size=18),legend.position="none")
+      
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax)) + 
+        geom_point(size=5,shape=18) + geom_errorbarh(size=1.2,height=0.5) +
+        theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+        labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+        theme(panel.background = element_rect(colour = "black"),legend.position="none",
+              axis.text=element_blank(),axis.title.y=element_blank(),
+              axis.title.x=element_text(face="bold",size=12)) + 
+        scale_color_manual(values=wes_palette("Zissou1",length(unique(data$obs2)),type="continuous"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
     }
     
     if(complete==TRUE){
@@ -102,16 +125,29 @@ plotRMSE <- function(data,data2=NULL,scen,type="simple",complete=FALSE){
         ggplot(data=data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2)) +
         geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
         coord_cartesian(ylim=yax) +
-        theme_classic() + scale_color_manual(values=wes_palette("Zissou1",length(unique(data$obs2)),type="continuous")) + 
-        theme(axis.text=element_text(size=13),axis.title=element_text(size=16)) + labs(x="Number of visits (J)",color=" Prop. of \nDouble Obs.")+#,title=names(data[i])) +
-        geom_errorbarh(data=optJ,mapping=aes(xmin=Jmin,xmax=Jmax,y=y,colour=obs2),inherit.aes=FALSE,size=1,height=my.scale/6)+
-        geom_text(data=optJ,mapping=aes(x=optJ,y=y,label="*",colour=obs2),inherit.aes=FALSE,size=12,fontface="bold",show.legend=F)
+        theme_classic() +
+        scale_color_manual(values=wes_palette("Zissou1",length(unique(data$obs2)),type="continuous")) + 
+        theme(axis.text=element_text(size=13),axis.title=element_text(size=16),legend.position="top") +
+        labs(x="Number of visits (J)",color=" Prop. of \nDouble Obs.")
+    
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax)) + 
+        geom_point(size=5,shape=18) + geom_errorbarh(size=1.2,height=0.5) +
+        theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+        labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+        theme(panel.background = element_rect(colour = "black"),legend.position="none",
+              axis.text=element_blank(),axis.title.y=element_blank(),
+              axis.title.x=element_text(face="bold",size=12)) + 
+        scale_color_manual(values=wes_palette("Zissou1",length(unique(data$obs2)),type="continuous"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
     }
     
     return(fig.out)
-  }
-  #rmseObs.fig(data=rel.rmse2,2)
-  } #type= "propObs"
+   }
+ #type= "propObs"
+
+
+# type="effortDiff", scenarios with different total effort (budget) -------
 
   if(type=="effortDiff"){
     
@@ -119,39 +155,45 @@ plotRMSE <- function(data,data2=NULL,scen,type="simple",complete=FALSE){
                data2[[scen]],
                data[[which(names(data) %in% names(data2)[scen])]]
              ),
-             effort=rep(c("4k","2k"),c(nrow(rel.rmse.eff2[[scen]]),
-                              nrow(rel.rmse2[[which(names(rel.rmse2) %in% names(rel.rmse.eff2)[scen])]])))
+             effort=rep(c("4k","2k"),c(nrow(data2[[scen]]),
+                              nrow(data[[which(names(data) %in% names(data2)[scen])]])))
              )
     
     # arranging needed information
-    my.scale <- (max(data$RMSE) - min(data$RMSE))*.3
-    yax <- c(min(data$RMSE)-my.scale, max(data$RMSE))
+    yax <- c(0, max(data$RMSE))
     
-    optJ <- data.frame(optJ=as.numeric(tapply(data$RMSE, list(data$obs2,data$effort),
-                                              function(x) as.character(data[data$RMSE==min(x),"J"]))),
-                       Jmin=as.numeric(tapply(data$RMSE, list(data$obs2,data$effort),
-                                              function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[1])),
-                       Jmax=as.numeric(tapply(data$RMSE, list(data$obs2,data$effort),
-                                              function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[2])),
-                       obs2=rep(unique(data$obs2),2),
-                       effort=rep(unique(data$effort),each=2),
-                       y=seq(yax[1],min(data$RMSE)-(my.scale/6),length.out=length(unique(data$obs2))*length(unique(data$effort)))
-    )
-    #optJ <- optJ[nrow(optJ):1,]
+    tab <- data.frame(optJ=tapply(data$RMSE, data$obs2,
+                                  function(x) as.numeric(as.character(data[data$RMSE==min(x),"J"]))),
+                      Jmin=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[1]),
+                      Jmax=tapply(data$RMSE, data$obs2,
+                                  function(x) range(as.numeric(as.character(data[which(x<min(x)+0.005),"J"])))[2]),
+                      obs2=unique(data$obs2),
+                      effort=rep(unique(data$effort),each=2))
     
     if(complete==FALSE){
-      optJ$y <- seq(0,min(data$RMSE)-.025,length.out=length(unique(data$obs2))*length(unique(data$effort)))
       # ggplot figure
       fig.out <-
         ggplot(data=data,aes(x=as.numeric(as.character(J)),y=RMSE,color=obs2,linetype=effort,group=interaction(obs2,effort))) +
         geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
         scale_linetype_manual(values=c("solid", "twodash")) + coord_cartesian(ylim=yax) +
         theme_classic() + scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
-        theme(axis.title=element_blank(),axis.text=element_text(size=18),legend.position="none") +
-        geom_errorbarh(data=optJ,mapping=aes(xmin=Jmin,xmax=Jmax,y=y,colour=obs2,linetype=effort),
-                       inherit.aes=FALSE,size=1,height=my.scale/4)+
-        geom_text(data=optJ,mapping=aes(x=optJ,y=y,label="*",colour=obs2),inherit.aes=FALSE,size=12,fontface="bold",show.legend=F)
-    }
+        theme(axis.title=element_blank(),axis.text=element_text(size=18),legend.position="none")
+      
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax,linetype=effort)) + 
+        geom_point(size=5,shape=18,position=position_dodge(0.9)) + 
+        geom_errorbarh(size=1.2,height=0.5,position=position_dodge(0.9)) +
+        theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+        labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+        theme(panel.background = element_rect(colour = "black"),legend.position="none",
+              axis.text=element_blank(),axis.title.y=element_blank(),
+              axis.title.x=element_text(face="bold",size=12)) + 
+        scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) +
+        scale_linetype_manual(values=c("solid", "twodash"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
+      
+     }
     
     if(complete==TRUE){
       # ggplot figure
@@ -160,17 +202,27 @@ plotRMSE <- function(data,data2=NULL,scen,type="simple",complete=FALSE){
         geom_line(size=1.6) + scale_x_continuous(breaks=2:30) +
         scale_linetype_manual(values=c("solid", "twodash")) + coord_cartesian(ylim=yax) +
         theme_classic() + scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) + 
-        theme(axis.text=element_text(size=13),axis.title=element_text(size=16)) +
-        labs(x="Number of visits (J)",color=" Model",linetype="Effort")+#,title=names(data[i])) +
-        geom_errorbarh(data=optJ,mapping=aes(xmin=Jmin,xmax=Jmax,y=y,colour=obs2,linetype=effort),
-                       inherit.aes=FALSE,size=1,height=my.scale/6)+
-        geom_text(data=optJ,mapping=aes(x=optJ,y=y,label="*",colour=obs2),inherit.aes=FALSE,size=12,fontface="bold",show.legend=F)
-    }
+        theme(axis.text=element_text(size=13),axis.title=element_text(size=16),legend.position="top") +
+        labs(x="Number of visits (J)",color=" Model",linetype="Effort")
+      
+      figJs <- ggplot(tab, aes(x=optJ,y=obs2,color=obs2,xmin=Jmin,xmax=Jmax,linetype=effort)) + 
+        geom_point(size=5,shape=18,position=position_dodge(0.9)) + 
+        geom_errorbarh(size=1.2,height=0.5,position=position_dodge(0.9)) +
+        theme_classic() + xlim(range(as.numeric(as.character(data$J)))) +
+        labs(x="Optimal J") + scale_y_discrete(limits=rev) +
+        theme(panel.background = element_rect(colour = "black"),legend.position="none",
+              axis.text=element_blank(),axis.title.y=element_blank(),
+              axis.title.x=element_text(face="bold",size=12)) + 
+        scale_color_manual(labels=c("1obs","2obs"),values=c("#3B9AB2","#F21A00")) +
+        scale_linetype_manual(values=c("solid", "twodash"))
+      
+      fig.out <- ggarrange(fig.out,figJs,nrow=2,align="v",heights=c(0.8,0.2))
+      
+      }
     
     return(fig.out)
-  }
-  #rmseObs.fig(data=rel.rmse2,2)
   }# type= "effortDiff"
+
   
 }# function
 
